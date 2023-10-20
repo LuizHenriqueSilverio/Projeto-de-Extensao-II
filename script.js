cf6630ca3287abdc2f8308c22019205ef392cf97
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-10-20 07:13:52 -0300

    Script atualizado.

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-function loadCSV() {
-    // Caminho para o arquivo CSV
-    const csvFilePath = 'actions.csv';
-    console.log("loadCSV");
-    // Use a biblioteca PapaParse para ler o CSV
-    Papa.parse(csvFilePath, {
-        header: true,
-        download: true,
-        dynamicTyping: true,
-        complete: function (results) {
-            const actions = results.data;
-            const tableBody = document.querySelector('#actions-table tbody');
-
-            // Limpe qualquer conteúdo existente na tabela
-            tableBody.innerHTML = '';
-
-            // Preencha a tabela com os dados do CSV
-            actions.forEach(function (action) {
-                const row = document.createElement('tr');
-                const nameCell = document.createElement('td');
-                const descriptionCell = document.createElement('td');
-
-                nameCell.textContent = action.Ação;
-                descriptionCell.textContent = action.Descrição;
-
-                row.appendChild(nameCell);
-                row.appendChild(descriptionCell);
-                tableBody.appendChild(row);
-            });
-        }
-    });
-}
-
-//window.addEventListener('load', loadCSV);
-
 // Função para carregar e exibir dados da planilha do Google Sheets
 function loadGoogleSheetData() {
     // ID da planilha do Google Sheets
@@ -68,13 +33,13 @@ function loadGoogleSheetData() {
 
 //Função para exibir mapa do Google
 function initMap() {
-    var coordenadas = { lat: -21.77656, lng: -45.84683 }; // Substitua com as coordenadas do local desejado
-    var mapa = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10, // Nível de zoom
+    const coordenadas = { lat: -21.77656, lng: -45.84683 }; 
+    const mapa = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15, // Nível de zoom
         center: coordenadas // Localização do mapa
     });
 
-    var marcador = new google.maps.Marker({
+    const marcador = new google.maps.Marker({
         position: coordenadas,
         map: mapa,
         title: "Local desejado"
