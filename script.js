f11f683345741cb762eb118cc350f8e8b41187a7
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-12-07 20:29:52 -0300

    Adicionado carregamento dinâmico nas página de notícias.

--- script.js
+++ script.js
@@ -31,21 +31,6 @@ function loadGoogleSheetData() {
     });
 }
 
-//Função para exibir mapa do Google
-function initMap() {
-    const coordenadas = { lat: -21.77656, lng: -45.84683 }; 
-    const mapa = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15, // Nível de zoom
-        center: coordenadas // Localização do mapa
-    });
-
-    const marcador = new google.maps.Marker({
-        position: coordenadas,
-        map: mapa,
-        title: "Local desejado"
-    });
-}
-
 // Função para inicializar a API do Google Sheets
 function initGoogleSheetsApi() {
     gapi.client.init({
@@ -56,8 +41,59 @@ function initGoogleSheetsApi() {
     });
 }
 
+// Função para carregar e exibir dados da planilha do Google Sheets para Noticias
+function loadGoogleSheetNews() {
+    // ID da planilha do Google Sheets
+    const spreadsheetId = '1Lh36jI-kUbPBr1ptiAAvZy7KE0UwRC4eXIGOz5JFmyU';
+    // Nome da aba para notícias
+    const sheetName = 'Noticias';
+
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: 'Noticias'
+    }).then(function(response) {
+        const data = response.result.values;
+        const newsSection = document.getElementById('news-section');
+
+        // Limpe qualquer conteúdo existente na seção de notícias
+        newsSection.innerHTML = '';
+
+        // Preencha a seção de notícias com os dados da planilha
+        data.forEach(function(row) {
+            const [title, newsDate, text, imageUrl] = row;
+
+            // Crie elementos HTML para cada notícia
+            const newsContainer = document.createElement('div');
+            newsContainer.classList.add('row', 'mb-5');
+
+            newsContainer.innerHTML = `
+                <div class="col-md-6">
+                    <h2>${title}</h2>
+                    <h5>${newsDate}</h5>
+                    <p>${text}</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="${imageUrl}" alt="${title}(imagem)" width="100%" height="100%">
+                </div>
+                <hr>
+            `;
+
+            // Adicione a notícia à seção de notícias
+            newsSection.appendChild(newsContainer);
+        });
+    });
+}
+
+// Inicialize a API do Google Sheets para Noticias
+function initGoogleSheetsApiForNews() {
+    gapi.client.init({
+        apiKey: 'AIzaSyDP2YPmfohOSkUf-OXM-0zRXcfM6IJCaho',
+        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function() {
+        loadGoogleSheetNews();
+    });
+}
+
 // Carrega a API do Google Sheets e inicia a aplicação
 gapi.load('client', initGoogleSheetsApi);
-
-
-
+gapi.load('client', initGoogleSheetsApiForNews);
