6ba50f3b1988df310c0a10c7ff883675dc9e72fe
LuizHenriqueSilverio <luizhenriquesilverios@hotmail.com>
2023-12-07 21:17:09 -0300

    Site finalizado.

---
+++ eventos.js
@@ -0,0 +1,45 @@
+// Função para carregar e exibir dados da planilha do Google Sheets
+function loadGoogleSheetData() {
+    // ID da planilha do Google Sheets
+    const spreadsheetId = '1NrKZXcG4BZ8YiVGgb7BkrDs5Cxvvn7xf1NyOKMswwC8';
+    // ID da planilha dentro do documento (geralmente 0 para a primeira planilha)
+    const sheetId = 0;
+
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: 'Eventos' // Substitua pelo nome da aba que você deseja ler
+    }).then(function(response) {
+        const data = response.result.values;
+        const tableBody = document.querySelector('#actions-table tbody');
+
+        // Limpe qualquer conteúdo existente na tabela
+        tableBody.innerHTML = '';
+
+        // Preencha a tabela com os dados da planilha
+        data.forEach(function(row) {
+            const rowData = row.map(item => item || ''); // Lida com valores nulos ou indefinidos
+
+            const tableRow = document.createElement('tr');
+            rowData.forEach(function(cellData) {
+                const cell = document.createElement('td');
+                cell.textContent = cellData;
+                tableRow.appendChild(cell);
+            });
+
+            tableBody.appendChild(tableRow);
+        });
+    });
+}
+
+// Função para inicializar a API do Google Sheets
+function initGoogleSheetsApi() {
+    gapi.client.init({
+        apiKey: 'AIzaSyDP2YPmfohOSkUf-OXM-0zRXcfM6IJCaho',
+        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function() {
+        loadGoogleSheetData();
+    });
+}
+
+// Carrega a API do Google Sheets e inicia a aplicação
+gapi.load('client', initGoogleSheetsApi);
--- noticias.js
+++ noticias.js
@@ -1,46 +1,3 @@
-// Função para carregar e exibir dados da planilha do Google Sheets
-function loadGoogleSheetData() {
-    // ID da planilha do Google Sheets
-    const spreadsheetId = '1NrKZXcG4BZ8YiVGgb7BkrDs5Cxvvn7xf1NyOKMswwC8';
-    // ID da planilha dentro do documento (geralmente 0 para a primeira planilha)
-    const sheetId = 0;
-
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: spreadsheetId,
-        range: 'Eventos' // Substitua pelo nome da aba que você deseja ler
-    }).then(function(response) {
-        const data = response.result.values;
-        const tableBody = document.querySelector('#actions-table tbody');
-
-        // Limpe qualquer conteúdo existente na tabela
-        tableBody.innerHTML = '';
-
-        // Preencha a tabela com os dados da planilha
-        data.forEach(function(row) {
-            const rowData = row.map(item => item || ''); // Lida com valores nulos ou indefinidos
-
-            const tableRow = document.createElement('tr');
-            rowData.forEach(function(cellData) {
-                const cell = document.createElement('td');
-                cell.textContent = cellData;
-                tableRow.appendChild(cell);
-            });
-
-            tableBody.appendChild(tableRow);
-        });
-    });
-}
-
-// Função para inicializar a API do Google Sheets
-function initGoogleSheetsApi() {
-    gapi.client.init({
-        apiKey: 'AIzaSyDP2YPmfohOSkUf-OXM-0zRXcfM6IJCaho',
-        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-    }).then(function() {
-        loadGoogleSheetData();
-    });
-}
-
 // Função para carregar e exibir dados da planilha do Google Sheets para Noticias
 function loadGoogleSheetNews() {
     // ID da planilha do Google Sheets
@@ -73,7 +30,7 @@ function loadGoogleSheetNews() {
                     <p>${text}</p>
                 </div>
                 <div class="col-md-6">
-                    <img src="${imageUrl}" alt="${title}(imagem)" width="100%" height="100%">
+                    <img src="./assets/img/frente_clube.jpg" alt="${title}(imagem)" width="100%" height="100%">
                 </div>
                 <hr>
             `;
@@ -95,5 +52,4 @@ function initGoogleSheetsApiForNews() {
 }
 
 // Carrega a API do Google Sheets e inicia a aplicação
-gapi.load('client', initGoogleSheetsApi);
 gapi.load('client', initGoogleSheetsApiForNews);
